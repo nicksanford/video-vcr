2cf9824f31feeae0e9764913b0957840077a3b8d
Nick Sanford <nick.sanford@viam.com>
2025-03-17 17:05:23 -0400

    wip

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Records rtsp video to a sqlite3 database.
 
 Useful for debugging
+
+### Build:
+```bash
+make
+```
+
+### Record:
+```
+./bin/linux-aarch64/vcr record rtsp://... recording.db
+```
+
+### Replay:
+```
+./bin/linux-aarch64/vcr replay recording.db
+```
--- cmd/vcr/main.go
+++ cmd/vcr/main.go
@@ -10,17 +10,17 @@ import (
 
 func main() {
 	if len(os.Args) != 3 && len(os.Args) != 4 {
-		fmt.Printf("usage: %s <record url|serve>  <sqlite.db>\n", os.Args[0])
+		fmt.Printf("usage: %s <record url|replay>  <sqlite.db>\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	switch path.Base(os.Args[1]) {
 	case "record":
 		client.Run(os.Args[2], os.Args[3])
-	case "serve":
+	case "replay":
 		server.Run(os.Args[2])
 	default:
-		fmt.Printf("usage: %s <client|server> <url> <sqlite.db>\n", os.Args[0])
+		fmt.Printf("usage: %s <client|replay> <url> <sqlite.db>\n", os.Args[0])
 		os.Exit(1)
 	}
 }
